4e624a307dc6dec9dd115423f1875b680e670823
Alex <alex@boltzbit.com>
2026-07-09 14:41:22 +0100

    Deploy: UK-wide copy

---
+++ _next/static/a66ZEuokd_6VPWCjom6HL/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/GovBid//_next/:path+",
+        "destination": "/GovBid/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
